3c2aa8adb9f89fb775348d3f97a151c244302384
Wellington Lopes <wellington.lopes.tw@gmail.com>
2020-03-19 13:30:41 -0400

    tipos

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <div class="conteudo">
                 <nav class="modulos">
                     <div class="modulo verde">
-                        <h3>Módulo 01</h3>
+                        <h3>Básico</h3>
                         <ul>
                             <li>
                                 <a href="exercicio.php?dir=basico&file=ola">
@@ -44,6 +44,51 @@
                             <li>
                                 <a href="exercicio.php?dir=basico&file=desafio">
                                     Desafio
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="modulo vermelho">
+                        <h3>Tipos</h3>
+                        <ul>
+                            <li>
+                                <a href="exercicio.php?dir=tipos&file=int">
+                                    Tipo Inteiro
+                                </a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=tipos&file=float">
+                                    Tipo Float
+                                </a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=tipos&file=aritmeticas">
+                                    Op. Aritméticas
+                                </a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=tipos&file=desafio_precedencia">
+                                    Desafio Precedência
+                                </a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=tipos&file=string">
+                                    Tipo String
+                                </a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=tipos&file=desafio_string">
+                                    Desafio String
+                                </a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=tipos&file=booleano">
+                                    Tipo Booleano
+                                </a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=tipos&file=conversoes">
+                                    Conversões
                                 </a>
                             </li>
                         </ul>
